447b67ffdbe9ab379898a57c939032689d768dbb
Ximsa <burrfelix@aol.de>
2021-09-21 15:29:27 +0200

    incemented version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject heightmap-converter "0.1.0-SNAPSHOT"
+(defproject heightmap-converter "0.1.1"
   :description "Converts a heightmap into the timberborn map format"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -8,6 +8,5 @@
   :main ^:skip-aot heightmap-converter.core
   :target-path "target/%s"
   :resource-paths ["resources"]
-  :clean-targets ^{:protect false} []
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
